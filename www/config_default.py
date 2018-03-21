6d0f8e7a8078fc9625468e92672ca0b49f857dc7
qipeng <qp19940413@163.com>
2018-03-21 17:53:10 +0800

    添加配置文件

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+# -- coding: utf-8 --
+
+# default config
+
+
+__author__ = 'Qp'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'admin',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+# -- coding: utf-8 --
+
+# Override configurations.
+
+
+__author__ = 'Qp'
+
+configs = {
+    'database': {
+        'host': '127.0.0.1'
+    }
+}
