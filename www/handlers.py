61ff98ac27058f18b5ce7b2debd6d50b30d06c44
qipeng <qp19940413@163.com>
2018-03-22 17:00:25 +0800

    添加用户登录功能

--- www/handlers.py
+++ www/handlers.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
+import hashlib
 import time
+import re
+import logging
+import json
 
+from aiohttp import web
+from apis import APIValueError, APIError
+from config import configs
 from coroweb import get
-from models import User
+from coroweb import post
+from models import User, next_id
 from models import Blog
 
 __author__ = 'Qp'
 
 ' url handlers '
+
+COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
 
 
 @get('/')
@@ -20,9 +31,124 @@ async def index(request):
         Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
     ]
 
-    user = await User.find(123)
+    user = await User.find(1)
     return {
         '__template__': 'blogs.html',
         'blogs': blogs,
         'user': user
     }
+
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHAI = re.compile(r'^[0-9a-f]{40}$')
+
+
+# 计算加密cookie
+def user2cookie(user, max_age):
+    """
+    Generate cookie str by user
+    :param user:
+    :param max_age:
+    :return:
+    """
+    # build cookie string by: id-expires-shal
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.password, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+
+# 解密cookie
+async def cookie2user(cookie_str):
+    """
+    Parse cookie and load user if cookie is valid
+    :param cookie_str:
+    :return:
+    """
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, shal = L
+        if int(expires) < time.time():
+            return None
+        user = await User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.password, expires, _COOKIE_KEY)
+        if shal != hashlib.shal(s.encode('utf-8')).hedigest():
+            logging.info('invalid sha1')
+            return None
+        user.password = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+
+# 用户注册
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+
+@post('/api/users')
+async def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHAI.match(passwd):
+        raise APIValueError('passwd')
+    users = await User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    shal_passwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email,
+                password=hashlib.sha1(shal_passwd.encode('utf-8')).hexdigest(),
+                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest)
+    await user.save()
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.password = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+
+@post('/api/authenticate')
+async def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password.')
+    users = await User.findAll('email=?', email)
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    # check password
+    shal = hashlib.sha1()
+    shal.update(user.id.encode('utf-8'))
+    shal.update(b':')
+    shal.update(passwd.encode('utf-8'))
+    if user.password != shal.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    # authenticate ok, set cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.password = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
